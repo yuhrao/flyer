c13c85918882028363b471e2aeb76c68327e1351
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2020-08-10 08:48:44 -0300

    Refac csv interceptor

--- src/flyer/server/common_interceptors.clj
+++ src/flyer/server/common_interceptors.clj
@@ -2,7 +2,6 @@
   (:require [io.pedestal.interceptor :as interceptor]))
 
 (defn route-csv-interceptor [file-path]
-  (interceptor/map->Interceptor
-   {::name ::add-file-path
-    :enter (fn [context]
-             (assoc context :file-path file-path))}))
+  {::name ::add-file-path
+   :enter (fn [context]
+            (assoc context :file-path file-path))})
--- test/flyer/server/common_interceptors_test.clj
+++ test/flyer/server/common_interceptors_test.clj
@@ -7,8 +7,8 @@
 (deftest csv-file-interceptor
   (let [csv-interceptor (common-interceptors/route-csv-interceptor "some-path")]
     (testing "should return a valid interceptor"
-      (is (match? interceptor/valid-interceptor?
-                  (-> csv-interceptor))))
+      (is (match? interceptor/interceptor?
+                  (interceptor/map->Interceptor csv-interceptor))))
     (testing "interceptor enter function should add file path to context map"
-      (is (match? {:csv-file-path string?}
+      (is (match? {:file-path string?}
                   ((:enter csv-interceptor) {}))))))
