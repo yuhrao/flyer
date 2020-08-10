4a9f1ed67dbf8d5207c39cb9e5d03ac63296dc33
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2020-08-10 20:20:31 -0300

    Add IO operation

---
+++ src/flyer/console/io.clj
@@ -0,0 +1,24 @@
+(ns flyer.console.io
+  (:require [clojure.string :as str]
+            [flyer.core.operations :as operations]))
+
+(defn ask [question]
+  (print question) 
+  (flush) 
+  (read-line))
+
+(defn valid-answer? [answer]
+  (->> (str/upper-case answer)
+       (re-matches #"(\w{3})[-](\w{3})")
+       boolean))
+
+(defn to-result-route-str
+  "Generate the result route in format '<path> R$<value>'"
+  [[path value]]
+  (let [path-str (->> path
+                      (map (comp str/upper-case name))
+                      (str/join  " - "))]
+    (format "%s > $%.2f" path-str value))) 
+
+(defn print-result [routing-result]
+  (print (to-result-route-str routing-result)))
---
+++ test/flyer/console/io_test.clj
@@ -0,0 +1,63 @@
+(ns flyer.console.io-test
+  (:require [flyer.console.io :as console]
+            [clojure.test :refer [deftest testing is]]
+            [matcher-combinators.test]
+            [matcher-combinators.matchers :as m]
+            [clojure.string :as str]))
+
+
+(deftest ask
+  (let [input "12"
+        question "6 + 6 is = "]
+    (testing "should return de provided input"
+      (is (match? input
+                  (with-in-str input (console/ask "some question:")))))
+    (testing "Should print the provided question"
+      (is (match? question
+                  (with-out-str (with-in-str input (console/ask question))))))))
+
+(deftest answer-validation
+(doseq [answer ["asd-fgh" "gru-cdg" "asd-asd"]]
+    (testing "Should be a valid answer"
+      (is (console/valid-answer? answer))))
+  (doseq [answer ["as-d" "asdf" "asd-asdf" "-assdhs"]]
+    (testing "Should be a invalid answer"
+      (is (not (console/valid-answer? answer))))))
+
+(deftest produce-result-route-string
+  (let [routing-path [:gru :brc :scl :orl :cdg]
+        routing-value 30.0
+        routing-result [routing-path routing-value]
+        result (console/to-result-route-str routing-result)]
+    (testing "Should produce a normalized result string"
+      (let [[path-str value-str] (str/split result #" > ")
+            value (-> value-str
+                      (str/replace #"\$" "")
+                      read-string
+                      float)
+            path (->> (str/split path-str #" - ")
+                      (map str/lower-case)
+                      (map keyword)
+                      (into []))]
+
+        (is (match? (m/equals [path value])
+                    routing-result))))))
+
+(deftest print-result-route
+  (let [routing-path [:gru :brc :scl :orl :cdg]
+        routing-value 30.0
+        routing-result [routing-path routing-value]
+        result (with-out-str (console/print-result routing-result))]
+    (testing "Should print a normalized result string"
+      (let [[path-str value-str] (str/split result #" > ")
+            value (-> value-str
+                      (str/replace #"\$" "")
+                      read-string
+                      float)
+            path (->> (str/split path-str #" - ")
+                      (map str/lower-case)
+                      (map keyword)
+                      (into []))]
+
+        (is (match? (m/equals [path value])
+                    routing-result))))))
