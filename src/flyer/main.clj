b5364e5636833437c2b665a8bbc5c31027bb4a91
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2020-08-10 21:13:31 -0300

    Create application entry point

--- src/flyer/main.clj
+++ src/flyer/main.clj
@@ -1,1 +1,14 @@
-(ns flyer.main)
+(ns flyer.main
+  (:gen-class)
+  (:require [flyer.console.main :as console-app]
+            [flyer.server.main :as flyer-server]
+            [flyer.server.routes.route :as route-routes]))
+
+(defonce server (atom nil))
+
+(defn -main [file-path]
+  (flyer-server/start! {:port 3000
+                        :routes [route-routes/routes]
+                        :file-path file-path}
+                       server)
+  (console-app/loop-requests file-path))
