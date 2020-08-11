d86c8b79aa9503c70fde8c9264da8badf2371ae1
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2020-08-10 21:13:23 -0300

    Add console operation

---
+++ src/flyer/console/main.clj
@@ -0,0 +1,30 @@
+(ns flyer.console.main
+  (:require [flyer.core.operations :as operations]
+            [flyer.console.io :as console]
+            [clojure.string :as str]))
+
+(defn- process-answer [answer]
+  (->> (str/split answer #"-")
+       (map str/lower-case)
+       (map keyword)))
+
+(defn ask-for-route []
+  (process-answer (console/ask "Please enter the route: ")))
+
+(defn request-route [file-path]
+  (let [[origin destination] (ask-for-route)
+        result (operations/get-best-route file-path origin destination)]
+    (if-not (nil? result)
+      (console/print-result result)
+      (console/print-not-found origin destination))))
+
+(defn loop-requests [file-path]
+  (while true
+    (request-route file-path)))
+
+(comment
+  (let [file "resources/input-file.txt"]
+    (request-route file)
+
+    #_(loop-requests "resources/input-file.txt"))
+  )
