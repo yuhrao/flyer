eaa5a68053e3e80f55c376289c33553c61c4eac7
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2020-08-14 11:57:14 -0300

    Remove core.file namespace

--- src/flyer/core/operations.clj
+++ src/flyer/core/operations.clj
@@ -2,14 +2,14 @@
   (:require [flyer.core.graph :as graph]
             [flyer.core.operations]
             [flyer.core.path-finder :as path-finder]
-            [flyer.core.file :as file]))
+            [flyer.file.core :as file]))
 
-(defn add-route! [file-path {:keys [origin destination value] :as route}]
+(defn ^:deprecated add-route! [file-path {:keys [origin destination value] :as route}]
   (let [csv-str (path-finder/to-csv-str route)]
     (file/write! file-path csv-str)
     csv-str))
 
-(defn get-best-route [file-path origin destination]
+(defn ^:deprecated get-best-route [file-path origin destination]
   (-> (file/read file-path)
       (graph/from-csv-string)
       (path-finder/build-best-route origin destination)))
